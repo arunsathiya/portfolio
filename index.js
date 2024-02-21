e7142510b39c2d8427f962bf3f8f5c15484465c1
Arun <arun@arun.blog>
2024-02-21 14:07:32 -0800

    Some progress with building posts from Notion

---
+++ index.js
@@ -0,0 +1,41 @@
+const { Client } = require("@notionhq/client");
+const { NotionToMarkdown } = require("notion-to-md");
+const fs = require("fs");
+// or
+// import {NotionToMarkdown} from "notion-to-md";
+
+const notion = new Client({
+  auth: "secret_redacted",
+});
+
+// passing notion client to the option
+const n2m = new NotionToMarkdown({
+  notionClient: notion,
+  config: {
+    parseChildPages: true,
+    separateChildPage: true,
+  },
+});
+
+(async () => {
+  const rootPageId = "af053880c57248e290c7ffefc4c88973";
+  const rootMdblocks = await n2m.pageToMarkdown(rootPageId);
+  const childPageBlocks = rootMdblocks.filter(
+    (block) => block.type === "child_page"
+  );
+
+  for (const block of childPageBlocks) {
+    const childPageId = block.blockId;
+    const childMdblocks = await n2m.pageToMarkdown(childPageId);
+    const mdString = n2m.toMarkdownString(childMdblocks);
+    const page = await notion.pages.retrieve({ page_id: childPageId });
+    const pageTitle = page.properties.title.title[0].plain_text.trimEnd();
+    const filePath = `./content/blog/${pageTitle}.md`;
+
+    const content = `---\nexternal: false\nnotion: ${childPageId}\ntitle: ${pageTitle}\ndescription: Auto-generated from Notion page\ndate: 2024-02-17\n---\n${mdString.parent.replace(
+      /\n\n/g,
+      "\n"
+    )}`;
+    fs.writeFileSync(filePath, content);
+  }
+})();
