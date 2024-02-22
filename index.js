645aac578bb7325267b1a7a951b4d81555129577
Arun <arun@arun.blog>
2024-02-22 00:40:55 -0800

    Rework Notion content build logic to fetch from a database instead of child pages of a root, thus making use of database page properties; also handle slugs as file names, title-styled

--- index.js
+++ index.js
@@ -13,30 +13,39 @@ const notion = new Client({
 const n2m = new NotionToMarkdown({
   notionClient: notion,
   config: {
-    parseChildPages: true,
-    separateChildPage: true,
+    parseChildPages: false,
+    separateChildPage: false,
   },
 });
 
+function normalizeString(str) {
+  return str.toLowerCase().replace(/\s+/g, "-");
+}
+
 (async () => {
-  const rootPageId = "af053880c57248e290c7ffefc4c88973";
-  const rootMdblocks = await n2m.pageToMarkdown(rootPageId);
-  const childPageBlocks = rootMdblocks.filter(
-    (block) => block.type === "child_page"
-  );
-
-  for (const block of childPageBlocks) {
-    const childPageId = block.blockId;
-    const childMdblocks = await n2m.pageToMarkdown(childPageId);
-    const mdString = n2m.toMarkdownString(childMdblocks);
-    const page = await notion.pages.retrieve({ page_id: childPageId });
-    const pageTitle = page.properties.title.title[0].plain_text.trimEnd();
-    const filePath = `./content/blog/${pageTitle}.md`;
-
-    const content = `---\nexternal: false\nnotion: ${childPageId}\ntitle: ${pageTitle}\ndescription: Auto-generated from Notion page\ndate: 2024-02-17\n---\n${mdString.parent.replace(
+  const databaseQuery = await notion.databases.query({
+    database_id: "8d62717492394debab4bea9262e3c066",
+  });
+  for (const page of databaseQuery.results) {
+    const pageId = page.id;
+    const mdblocks = await n2m.pageToMarkdown(pageId);
+    const mdString = n2m.toMarkdownString(mdblocks);
+    const pageInfo = await notion.pages.retrieve({ page_id: pageId });
+    const pageTitle = pageInfo.properties.Title.title[0].plain_text.trimEnd();
+    const pageSlug = pageInfo.properties.Slug.rich_text[0].plain_text.trimEnd();
+    const pureSlug = normalizeString(
+      pageInfo.properties.Slug.rich_text[0].plain_text.trimEnd()
+    );
+    const pageDescription =
+      pageInfo.properties.Description.rich_text[0].plain_text.trimEnd();
+    const pageDate = pageInfo.properties.Date.date.start;
+    const filePath = `./content/blog/${pageSlug}.md`;
+    const content = `---\nexternal: false\nnotion: ${pageId}\ntitle: ${pageTitle}\nslug: ${pureSlug}\ndescription: ${pageDescription}\ndate: ${pageDate}\n---\n${mdString.parent.replace(
       /\n\n/g,
       "\n"
     )}`;
     fs.writeFileSync(filePath, content);
   }
 })();
+
+module.exports = { normalizeString };
