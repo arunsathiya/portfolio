5bf2bc53b99aa6935af39e792845ef58ace93fbd
Arun <arun@arun.blog>
2024-08-23 16:33:28 -0700

    fix: update site constants to reflect personal branding

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Devolio';
-export const SITE_TAGLINE = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site tagline
-export const SITE_DESCRIPTION = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site meta description for SEO
+export const SITE_TITLE = 'Arun';
+export const SITE_TAGLINE = 'Software Engineer with focus on data-driven applications with Golang, TypeScript and Python';
+export const SITE_DESCRIPTION = 'I\'m Arun, a full-stack engineer with fresh Data Analytics expertise, crafting efficient solutions in Golang, TypeScript, and Python.My focus spans data-driven applications, serverless architectures, and DevOps practices.';
