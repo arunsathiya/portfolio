88d334a16dba792402ab4b5140b699e03aed19a7
Arun <arun@arun.blog>
2024-08-23 16:34:14 -0700

    fix: update site tagline to improve clarity

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Arun';
-export const SITE_TAGLINE = 'Software Engineer with focus on data-driven applications with Golang, TypeScript and Python';
+export const SITE_TAGLINE = 'Software Engineer with focus on data-driven applications';
 export const SITE_DESCRIPTION = 'I\'m Arun, a full-stack engineer with fresh Data Analytics expertise, crafting efficient solutions in Golang, TypeScript, and Python.My focus spans data-driven applications, serverless architectures, and DevOps practices.';
