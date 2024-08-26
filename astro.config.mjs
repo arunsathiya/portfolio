49b1a1619d5a3899a2b65e15080b9a01776a4932
Arun <arun@arun.blog>
2024-08-26 02:08:53 -0700

    debug: undo node changes previously made for cloudflare support

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import { autoNewTabExternalLinks } from './src/autoNewTabExternalLinks';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.arun.blog',
@@ -23,37 +21,5 @@ export default defineConfig({
 			],
 		],
 	},
-	adapter: cloudflare({
-		routes: {
-			extend: {
-				include: ['/api/*'],
-			},
-		},
-		platformProxy: {
-			enabled: true,
-		},
-	}),
-	vite: {
-		ssr: {
-			external: [
-				'node:fs',
-				'node:fs/promises',
-				'node:path',
-				'node:url',
-				'node:assert',
-				'node:async_hooks',
-				'node:buffer',
-				'node:crypto',
-				'node:diagnostics_channel',
-				'node:events',
-				'node:process',
-				'node:stream',
-				'node:string_decoder',
-				'node:test',
-				'node:util',
-				'node:perf_hooks',
-				'node:module',
-			],
-		},
-	},
+	adapter: vercel(),
 });
