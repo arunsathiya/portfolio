6e49ee2490a2dfd4c0ed44703d0f26d1aa1e1066
Arun <arun@arun.blog>
2024-08-26 10:47:17 -0700

    fix: update tagline and do not show it on the index page title

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Arun';
-export const SITE_TAGLINE = 'Software Engineer with focus on data-driven applications';
+export const SITE_TAGLINE = 'Pushing Boundaries';
 export const SITE_DESCRIPTION =
 	"I'm Arun, a full-stack engineer with fresh Data Analytics expertise, crafting efficient solutions in Golang, TypeScript, and Python.My focus spans data-driven applications, serverless architectures, and DevOps practices.";
