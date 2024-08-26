11b60ad3dffe56b4dfbeaac66c735dbbacf49a2c
Arun <arun@arun.blog>
2024-08-26 04:43:36 -0700

    feat: blog prefix to match the behavior I had for a long time

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
@@ -10,7 +10,7 @@ export async function GET(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/${post.slug}/`,
+			link: `/blog/${post.slug}/`,
 		})),
 	});
 }
