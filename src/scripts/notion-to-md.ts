8b6bf4922a54541da1ba4a7b65572321b317a74e
Arun <arun@arun.blog>
2024-09-02 07:25:51 -0700

    fix: show published date in UTC, to match the identifier version that I have in folder names in the form of date-slug combinations

--- src/scripts/notion-to-md.ts
+++ src/scripts/notion-to-md.ts
@@ -23,9 +23,9 @@ const n2m = new NotionToMarkdown({
 
 function formatDate(dateString: string): string {
 	const date = new Date(dateString);
-	const month = date.toLocaleString('default', { month: 'short' });
-	const day = date.getDate().toString().padStart(2, '0');
-	const year = date.getFullYear();
+	const month = date.toLocaleString('default', { month: 'short', timeZone: 'UTC' });
+	const day = date.getUTCDate().toString().padStart(2, '0');
+	const year = date.getUTCFullYear();
 	return `${month} ${day} ${year}`;
 }
 
