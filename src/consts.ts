010c28a9c2a79f60231938d2181310429527d656
Arun <arun@arun.blog>
2024-12-24 02:03:38 +0530

    chore: remove open-to-work notice

--- src/consts.ts
+++ src/consts.ts
@@ -4,4 +4,4 @@
 export const SITE_TITLE = 'Arun';
 export const SITE_TAGLINE = 'Pushing Boundaries';
 export const SITE_DESCRIPTION =
-	"I'm Arun, a full-stack engineer with fresh Data Analytics expertise, crafting efficient solutions in Golang, TypeScript, and Python.My focus spans data-driven applications, serverless architectures, and DevOps practices.";
+	"I'm Arun, a full-stack engineer with Data Analytics expertise, crafting efficient solutions in Golang, TypeScript, and Python.My focus spans data-driven applications, serverless architectures, and DevOps practices.";
